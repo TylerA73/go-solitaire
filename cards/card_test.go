050d9fdfe60c03f6200c797c692f7aac722e7cf6
TylerA73 <tylera73@gmail.com>
2021-12-18 11:43:37 -0700

    Test

---
+++ cards/card_test.go
@@ -0,0 +1,13 @@
+package cards
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test(t *testing.T) {
+	t.Run("Should pass", func(t *testing.T) {
+		assert.Fail(t, "Fail")
+	})
+}
