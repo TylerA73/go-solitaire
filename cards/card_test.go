90dce1b8902ec61a42aef4cfb8995d4f02632f38
TylerA73 <tylera73@gmail.com>
2021-12-18 18:11:03 -0700

    Add card functionality and unit tests

--- cards/card_test.go
+++ cards/card_test.go
@@ -3,11 +3,50 @@ package cards
 import (
 	"testing"
 
+	"github.com/TylerA73/go-solitaire/suits"
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	c1 *Card = NewCard(5, &suits.Hearts)
+)
+
 func Test(t *testing.T) {
-	t.Run("Should pass", func(t *testing.T) {
-		assert.True(t, true)
+	t.Run("should return true if card suit is red", func(t *testing.T) {
+		assert.True(t, c1.IsRed())
+	})
+
+	t.Run("should return false if the suit is not red", func(t *testing.T) {
+		c2 := NewCard(5, &suits.Clubs)
+
+		assert.False(t, c2.IsRed())
+	})
+
+	t.Run("should return true if c2 and c3 can be stacked on top of c1 in a pile", func(t *testing.T) {
+		c2 := NewCard(4, &suits.Clubs)
+		c3 := NewCard(4, &suits.Spades)
+
+		assert.True(t, c1.CanStackOnPile(c2))
+		assert.True(t, c1.CanStackOnPile(c3))
+	})
+
+	t.Run("should return false if c2 and c3 can't be stacked on top of c1 in a pile", func(t *testing.T) {
+		c2 := NewCard(8, &suits.Clubs)
+		c3 := NewCard(8, &suits.Diamonds)
+
+		assert.False(t, c1.CanStackOnPile(c2))
+		assert.False(t, c1.CanStackOnPile(c3))
+	})
+
+	t.Run("should return true if c2 can be stacked on top of c1 in a home pile", func(t *testing.T) {
+		c2 := NewCard(6, &suits.Hearts)
+
+		assert.True(t, c1.CanStackOnHome(c2))
+	})
+
+	t.Run("should return false if c2 can't be stacked on top of c1 in a home pile", func(t *testing.T) {
+		c2 := NewCard(6, &suits.Clubs)
+
+		assert.False(t, c1.CanStackOnHome(c2))
 	})
 }
