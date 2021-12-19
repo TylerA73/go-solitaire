74168830a42f3cfa59b2516f1f263311b866c422
TylerA73 <tylera73@gmail.com>
2021-12-18 18:11:53 -0700

    Add card functionality and unit tests (#4)

--- cards/card.go
+++ cards/card.go
@@ -1,1 +1,35 @@
 package cards
+
+import (
+	"github.com/TylerA73/go-solitaire/suits"
+)
+
+type Card struct {
+	Ordinal   int
+	Suit      *suits.Suit
+	IsFlipped bool
+}
+
+func NewCard(ordinal int, suit *suits.Suit) *Card {
+	return &Card{
+		Ordinal:   ordinal,
+		Suit:      suit,
+		IsFlipped: false,
+	}
+}
+
+func (c *Card) IsRed() bool {
+	return c.Suit.IsRed
+}
+
+func (c *Card) CanStackOnPile(card *Card) bool {
+	return c.Ordinal == card.Ordinal+1 && c.IsRed() != card.IsRed()
+}
+
+func (c *Card) CanStackOnHome(card *Card) bool {
+	return c.Ordinal == card.Ordinal-1 && c.Suit.IsEqual(card.Suit)
+}
+
+func (c *Card) Flip() {
+	c.IsFlipped = !c.IsFlipped
+}
